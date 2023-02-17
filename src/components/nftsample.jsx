485b5dfd44b7581d61f87410f6a6e8566265fe18
chattertechno <reneekrom28@gmail.com>
2023-02-17 23:43:35 +0300

    add characters

--- src/components/nftsample.jsx
+++ src/components/nftsample.jsx
@@ -5,9 +5,16 @@ import Queen from "../assets/images/queen.png";
 import pawn from "../assets/images/pawn.png";
 import king from "../assets/images/King1.png";
 import knight from "../assets/images/Knight.png";
+import bishop from "../assets/images/Bishop1.png"
+import newKing from "../assets/images/king.png"
+import newQueen from "../assets/images/Queen1.png"
+import newRook from "../assets/images/Rook3.png"
+import newKnight from "../assets/images/Knight1.png"
+import newBishop from "../assets/images/Bishop2.png"
+import newPawn from "../assets/images/Pawn1.png"
 import "./arena.css";
 
-const imagelia = [Rook, Queen, pawn, king, knight]
+const imagelia = [Rook, Queen, pawn, king, knight, bishop, newKing, newKnight, newQueen, newRook, newBishop, newPawn]
 const REPEAT_ARR = Array.from(Array(4).keys());
 
 export default function Nftsample() {
